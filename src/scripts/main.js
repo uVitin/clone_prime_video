4f502768b5287823e7a2bf29152d4dae4d03392b
Albert Vitor <albertvitor18@outlook.com>
2024-07-27 13:55:59 -0300

    Projeto Finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
-function test() {
-    console.log('ola mundo')
+document.addEventListener('DOMContentLoaded', function() {
+    const headerSection = document.querySelector('.hero__content');
+    const alturaHeader = headerSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+        
+        if (posicaoAtual < alturaHeader) {            
+            exibiElementosDoHeader();
+        } else {
+            ocultaElementosDoHeader();
+        }
+    })
+})
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('.header__container');
+    header.classList.add('--hidden')
 }
+
+function exibiElementosDoHeader() {
+    const header = document.querySelector('.header__container');
+    header.classList.remove('--hidden')
+}
+
